4350d395d515a2eee9edb7cded1f8cb150a42037
Sara <s.p.gajda@gmail.com>
2020-01-08 19:46:49 -0600

    created 2 lists of movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Movie } from './movie';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies';
+
+  watched = [
+    new Movie("Knives Out", "Drama", 2019),
+    new Movie("Rise of Skywalker", "Action", 2019)
+  ];
+
+  toWatch = [
+    new Movie("some movie", "comedy", 2020),
+    new Movie("another movie", "another comedy", 2030)
+  ];
 }
---
+++ src/app/movie.ts
@@ -0,0 +1,12 @@
+export class Movie {
+    title: string;
+    category: string;
+    year: number;
+
+    constructor(title: string, category: string, year: number) {
+        this.title = title;
+        this.category = category;
+        this.year = year;
+    }
+}
+
